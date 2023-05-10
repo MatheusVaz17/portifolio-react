db207dc9b5cecdc2a1f855947af5b0aa5721085b
MatheusVaz17 <matheusvaz765@gmail.com>
2023-05-10 00:16:58 -0300

    Collapse descrição dos cards habilidades

--- src/components/Skills/index.js
+++ src/components/Skills/index.js
@@ -1,72 +1,85 @@
-import React, { useEffect, useState } from "react";
-import {Card} from 'primereact/card';
-import {SiJavascript, SiBootstrap, SiPhp, SiLaravel, SiMysql} from 'react-icons/si';
-import {FaReact} from 'react-icons/fa';
-import {DiYii} from 'react-icons/di';
-import {BiGitBranch, BiChip} from 'react-icons/bi';
+import React, { useCallback, useEffect, useState } from "react";
+import * as S from './styles';
+import { Accordion, AccordionTab } from 'primereact/accordion';
 
 const Skills = () => {
 
+    const handleClick = (event, cardId) => {
+        console.log(cardId);
+        var descriptionCard = document.getElementById(cardId);
+        descriptionCard.style.display = 'flex';
+      };
+
     const titleFrontEnd = (
-        <>
-            FRONTEND <SiJavascript /> <SiBootstrap /> <FaReact />
-        </>
+        <S.StyledTitleCard>FRONTEND</S.StyledTitleCard>
     );
 
     const titleBackEnd = (
-        <>
-            BACKEND <SiPhp /><DiYii/><SiLaravel/>
-        </>
+        <S.StyledTitleCard>BACKEND</S.StyledTitleCard>
     );
 
     const titleOthers = (
+        <S.StyledTitleCard>OUTROS</S.StyledTitleCard>
+    );
+
+    const arrowIcon =(
         <>
-            OUTROS <SiMysql /><BiGitBranch /><BiChip />
+            <S.StyledArrowDownIcon />
         </>
     );
+
+    const footerCards = (cardId) => {
+    
+        return <S.StyledButtonArrow label={arrowIcon} onClick={(e) => handleClick(e, cardId)} className="btnFooterCard -close" severity="secondary" size="small" text raised rounded />
+        
+    };
 
     return (
         <>
             <div className="grid" style={{display: 'contents'}}>
 
                 <div className="col-12 sm:col-12 md:col-12 lg:col-12 xl:col-12" style={{width: '100%', display: 'flex', padding: '0px'}}>
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#000000" fill-opacity="1" d="M0,96L30,90.7C60,85,120,75,180,96C240,117,300,171,360,165.3C420,160,480,96,540,96C600,96,660,160,720,165.3C780,171,840,117,900,128C960,139,1020,213,1080,213.3C1140,213,1200,139,1260,106.7C1320,75,1380,85,1410,90.7L1440,96L1440,320L1410,320C1380,320,1320,320,1260,320C1200,320,1140,320,1080,320C1020,320,960,320,900,320C840,320,780,320,720,320C660,320,600,320,540,320C480,320,420,320,360,320C300,320,240,320,180,320C120,320,60,320,30,320L0,320Z"></path></svg>
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#00000e" d="M0,96L30,90.7C60,85,120,75,180,96C240,117,300,171,360,165.3C420,160,480,96,540,96C600,96,660,160,720,165.3C780,171,840,117,900,128C960,139,1020,213,1080,213.3C1140,213,1200,139,1260,106.7C1320,75,1380,85,1410,90.7L1440,96L1440,320L1410,320C1380,320,1320,320,1260,320C1200,320,1140,320,1080,320C1020,320,960,320,900,320C840,320,780,320,720,320C660,320,600,320,540,320C480,320,420,320,360,320C300,320,240,320,180,320C120,320,60,320,30,320L0,320Z"></path></svg>
                 </div>
                 
-                <div className="card" style={{width: '100%', backgroundColor: 'black'}}>
+                <div className="card" style={{width: '100%', backgroundColor: '#00000e'}}>
 
                     <div className="card-container">
 
                         <div className="inline-block col-12">
-                            <Card>
+                            <S.StyledCardSkills>
 
-                            <h1>HABILIDADES</h1>
+                            <h1 style={{color: 'white'}}>HABILIDADES</h1>
 
                             <div className="inline-block col-12 sm:col-12 md:col-4 lg:col-4 xl:col-4">
-                                <Card title={titleFrontEnd} style={{boxShadow: '7px 7px gray', border: '1px solid darkgray'}}>
-                                    <p className="m-0">
-                                        
+                                <S.StyledCard className="cardFront" title={titleFrontEnd} footer={footerCards('descriptionFront')}>
+                                    <h1><S.StyledJsIcon /> <S.StyledBootstrapIcon /> <S.StyledReactIcon /></h1>
+                                    <p className="m-0" id="descriptionFront" style={{display: 'none'}}>
+                                        DESCRIÇÃO FRONT END
                                     </p>
-                                </Card>
+
+                                </S.StyledCard>
                             </div>
 
                             <div className="inline-block col-12 sm:col-12 md:col-4 lg:col-4 xl:col-4">
-                                <Card title={titleBackEnd} style={{boxShadow: '7px 7px gray', border: '1px solid darkgray'}}>
-                                    <p className="m-0">
-                                        
+                                <S.StyledCard className="cardBack" title={titleBackEnd} footer={footerCards('descriptionBack')}>
+                                    <h1><S.StyledPhpIcon /><S.StyledYiiIcon /><S.StyledLaravelIcon/></h1>
+                                    <p className="m-0" id="descriptionBack" style={{display: 'none'}}>
+                                        DESCRIÇÃO BACK END
                                     </p>
-                                </Card>
+                                </S.StyledCard>
                             </div>
 
                             <div className="inline-block col-12 sm:col-12 md:col-4 lg:col-4 xl:col-4">
-                                <Card title={titleOthers} style={{boxShadow: '7px 7px gray', border: '1px solid darkgray'}}>
-                                    <p className="m-0">
-                                        
+                                <S.StyledCard className="cardOthers" title={titleOthers} footer={footerCards('descriptionOthers')}>
+                                    <h1><S.StyledMysqlIcon /><S.StyledGitIcon /><S.StyledChipIcon /></h1>
+                                    <p className="m-0" id="descriptionOthers" style={{display: 'none'}}>
+                                        DESCRIÇÃO OUTROS
                                     </p>
-                                </Card>
+                                </S.StyledCard>
                             </div>
 
-                            </Card>
+                            </S.StyledCardSkills>
                         </div>
                         
                     </div>
---
+++ src/components/Skills/styles.js
@@ -0,0 +1,82 @@
+import styled from "styled-components";
+import {Card} from "primereact/card";
+import {SiJavascript, SiBootstrap, SiPhp, SiLaravel, SiMysql} from 'react-icons/si';
+import {FaReact} from 'react-icons/fa';
+import {DiYii} from 'react-icons/di';
+import {BiGitBranch, BiChip} from 'react-icons/bi';
+import {AiOutlineArrowDown} from 'react-icons/ai';
+import {Button} from 'primereact/button';
+
+export const StyledJsIcon = styled(SiJavascript)`
+    fill: #03c4e8;
+`;
+
+export const StyledCardSkills = styled(Card)`
+    background-color: #00000e;
+`;
+
+export const StyledBootstrapIcon = styled(SiBootstrap)`
+    fill: #03c4e8;
+`;
+
+export const StyledReactIcon = styled(FaReact)`
+    fill: #03c4e8;
+`;
+
+export const StyledPhpIcon = styled(SiPhp)`
+    fill: #03c4e8;
+`;
+
+export const StyledYiiIcon = styled(DiYii)`
+    fill: #03c4e8;
+`;
+
+export const StyledLaravelIcon = styled(SiLaravel)`
+    fill: #03c4e8;
+`;
+
+export const StyledMysqlIcon = styled(SiMysql)`
+    fill: #03c4e8;
+`;
+
+export const StyledGitIcon = styled(BiGitBranch)`
+    fill: #03c4e8;
+`;
+
+export const StyledChipIcon = styled(BiChip)`
+    fill: #03c4e8;
+`;
+
+export const StyledButtonArrow = styled(Button)`
+    &:hover{
+        color: white;
+    }
+`;
+
+export const StyledArrowDownIcon = styled(AiOutlineArrowDown)`
+`;
+
+export const StyledTitleCard = styled.div`
+    font-size: 25px;
+`;
+
+export const StyledCard = styled(Card)`
+    height: auto;
+    border-radius: 20px;
+    box-shadow: 7px 7px gray;
+    border: 1px solid darkgray;
+    &:hover{
+        background-color: gray;
+        box-shadow: 7px 7px white;
+        border: none;
+        ${StyledJsIcon}, ${StyledBootstrapIcon}, ${StyledReactIcon}, ${StyledPhpIcon}, ${StyledYiiIcon}, ${StyledLaravelIcon}, ${StyledMysqlIcon}, ${StyledGitIcon}, ${StyledChipIcon} {
+            fill: white;
+        }
+        ${StyledButtonArrow} {
+            background-color: white;
+        }
+        ${StyledTitleCard} {
+            color: white
+        }
+    }
+`;
